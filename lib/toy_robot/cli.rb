12c641759a93dcd146ec00c729d012523d326023
Bruno Sapienza <bruno.sapienza@gmail.com>
2016-09-19 00:06:54 -0300

    Refactor CLI

--- lib/toy_robot/cli.rb
+++ lib/toy_robot/cli.rb
@@ -1,14 +1,27 @@
 # frozen_string_literal: true
+
 module ToyRobot
   class CLI < Thor
     desc 'execute', 'executes given commands'
 
     option :instructions_file,
            required: true,
-           banner: './example-data/instructions-1.txt'
+           banner: './example_data/instructions_1.txt'
 
     def execute
-      'Hello World!'
+      CommandCenter.new(commands, table).execute
+
+      true
+    end
+
+    private
+
+    def table
+      @table = Table.new(x_range: 5, y_range: 5)
+    end
+
+    def commands
+      @commands ||= Parser.new(options[:instructions_file]).parse
     end
   end
 end
--- spec/toy_robot/cli_spec.rb
+++ spec/toy_robot/cli_spec.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
+
 describe ToyRobot::CLI do
-  describe '#hello_world' do
-    # let(:options) { { instructions_file: '' } }
+  describe '#execute' do
     subject(:cli) { described_class.new }
     subject(:execute) { cli.execute }
 
-    it { is_expected.to eq('Hello World!') }
+    before do
+      allow_any_instance_of(ToyRobot::Parser).to receive(:parse)
+      allow_any_instance_of(ToyRobot::CommandCenter).to receive(:execute)
+    end
+
+    it { is_expected.to be_truthy }
   end
 end
